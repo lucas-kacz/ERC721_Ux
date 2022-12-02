5b6e499ebdf90e62805f9a34f97909d6ae7d0478
Lucas Kaczmarski <lucaskacz1@gmail.com>
2022-12-02 20:24:13 +0100

    Update Front, connect to Metamask

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,9 +1,51 @@
-
+import { useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./components/Routes";
+import Login from "./components/pages/Login/Login";
+import Home from "./components/pages/Home/Home";
+import Web3 from "web3";
 
 function App() {
+
+  const [isConnected, setIsConnected] = useState(false);
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+  const onLogin = async (provider) => {
+    const web3 = new Web3(provider);
+    const accounts = await web3.eth.getAccounts()
+    if (accounts.length === 0){
+      console.log("Please connect to Metamask")
+    }
+    else if (accounts[0] !== currentAccount){
+      setCurrentAccount(accounts[0]);
+      setIsConnected(true);
+    }
+  };
+
+  const onLogout = () => {
+    setIsConnected(false);
+  };
+
   return (
     <div className="App">
-      Hello
+      <header className="main-header">
+        <h1>ERC721_UX</h1>
+        <nav className="nav">
+          <ul>
+            <li>
+              <a href="/">{currentAccount}</a>
+            </li>
+          </ul>
+          </nav>
+      </header>
+      <main>
+        <Router>
+          <AppRoutes />
+        </Router>
+
+        {!isConnected && <Login onLogin={onLogin} onLogout={onLogout}/>}
+        {isConnected && <Home currentAccount={currentAccount} />}
+      </main>
     </div>
   );
 }
---
+++ my_app/src/components/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import Card from "../Card/Card";
+import classes from "./Home.module.css";
+
+const Home = (props) => {
+    return ( 
+        <Card className={classes.home}>
+            <h1>Welcome</h1>
+            <p>{props.currenAccount}</p>
+        </Card>
+    );
+};
+
+export default Home;
